8fd33624cd8a90276e96bcf087b1c5f3e558cb85
Bogdan Popa <bogdan@defn.io>
2020-10-15 11:55:39 +0300

    ci: install packages in installation scope

--- ci/setup-catalogs.sh
+++ ci/setup-catalogs.sh
@@ -2,7 +2,18 @@
 
 set -euo pipefail
 
-raco pkg config --set catalogs \
-    https://download.racket-lang.org/releases/7.8/catalog/ \
-    https://racksnaps.defn.io/built-snapshots/2020/10/01/catalog/ \
-    https://racksnaps.defn.io/snapshots/2020/10/01/catalog/
+echo "Setting up installation catalogs..."
+raco pkg config  \
+     --installation \
+     --set catalogs \
+     https://download.racket-lang.org/releases/7.8/catalog/ \
+     https://racksnaps.defn.io/built-snapshots/2020/10/01/catalog/ \
+     https://racksnaps.defn.io/snapshots/2020/10/01/catalog/
+
+echo "Setting up user catalogs..."
+raco pkg config \
+     --user \
+     --set catalogs \
+     https://download.racket-lang.org/releases/7.8/catalog/ \
+     https://racksnaps.defn.io/built-snapshots/2020/10/01/catalog/ \
+     https://racksnaps.defn.io/snapshots/2020/10/01/catalog/
