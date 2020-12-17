2754d6ddc8903f74dd12e314550ba89305be3a38
Marc Kaufmann <marc.kaufmannmk@gmail.com>
2020-12-17 08:36:28 +0100

    pj-study requires price-list-study

--- congame-pjb-studies/info.rkt
+++ congame-pjb-studies/info.rkt
@@ -2,7 +2,8 @@
 
 (define collection "congame-pjb-studies")
 (define deps '("base"
-               "congame-core"))
+               "congame-core"
+               "congame-price-lists"))
 (define build-deps '())
 (define congame-studies
   '((congame-pjb-studies/pjb-pilot pjb-pilot-study)))
