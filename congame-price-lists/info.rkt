7d4cf32690eb8a0c564e42cc6ac5b3f90d95924d
Bogdan Popa <bogdan@defn.io>
2020-12-18 19:48:30 +0200

    build: add missing deps to congame-price-lists

--- congame-price-lists/info.rkt
+++ congame-price-lists/info.rkt
@@ -2,7 +2,9 @@
 
 (define collection "congame-price-lists")
 (define deps '("base"
-               "congame-core"))
+               "congame-core"
+               "forms-lib"
+               "koyo-lib"))
 (define build-deps '())
 (define congame-studies
   '((congame-price-lists/price-lists pl-study)))
