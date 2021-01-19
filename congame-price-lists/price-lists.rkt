1baa702bf13d68d4b058910245decf96232a62f9
Marc Kaufmann <marc.kaufmannmk@gmail.com>
2021-01-19 18:06:53 +0100

    price-lists: redesign data structure, bot, and render

--- congame-price-lists/price-lists.rkt
+++ congame-price-lists/price-lists.rkt
@@ -5,32 +5,29 @@
          marionette
          racket/format
          racket/generic
-         racket/list
          racket/match
-         racket/random
          racket/serialize
          (prefix-in bot: (submod congame/components/bot actions))
+         (submod congame/components/bot actions)
+         congame/components/bot-maker
          congame/components/export
-         congame/components/registry
          congame/components/study)
 
-;;; TODO:
-;;; This is designed the wrong way. Goals:
-;;; Provide price-list-step that creates a step for general types of price-lists
-;;; It has to have options for `fixed`, `variable`, `from`, `to`, and `increment`
-;;; Increment should be a function that can increment.
-;;; On the other hand, studies have the benefits of defining their #:requires and #:provides,
-;;; which allows for some type checking, whereas a step does not. Why do I care? Because of
-;;; hooking steps up with models and types of data we want to collect. It may be possible
-;;; to test steps as well. Or not, since steps don't have return values anymore.
+;;; TODO: Should we provide just pl-step or pl-study? The latter defines #:requires and #:provides,
+;;; which allows for some type checking, unlike steps. Useful for configuring via symbolic models
+;;; via types of data to collect. Can this be done via steps - certainly if we define a
+;;; generice `step->study`.
+;;; TODO: provide a `make-pl` function for other modules to use
 
 (provide
- pl-study)
+ price-list-step
+ price-list-step/bot
+ pl-study
+ make-pl)
 
 (define-generics describable
   (describe describable))
 
-;; TODO: Work time VS decision time
 (serializable-struct option (work money)
   #:transparent
   #:methods gen:describable
@@ -43,6 +40,16 @@
      (hash 'type "option"
            'work (option-work o)
            'money (option-money o)))])
+
+(serializable-struct adjustable-option (work)
+  #:transparent
+  #:methods gen:jsexprable
+  [(define (->jsexpr v)
+     (hash 'type "adjustable-option"
+           'work (adjustable-option-work v)))])
+
+(define (set-level/adjustable a l)
+  (option (adjustable-option-work a) l))
 
 (serializable-struct option/timed (work money work-time decision-time)
   #:transparent
@@ -71,106 +78,100 @@
            'chose (->jsexpr/super (choice-chosen c))
            'options (->jsexpr/super (choice-options c))))])
 
-(serializable-struct price-list (treatment choices)
+(serializable-struct price-list (name fixed adjustable levels answers)
   #:transparent
   #:methods gen:jsexprable
   [(define/generic ->jsexpr/super ->jsexpr)
    (define (->jsexpr pl)
      (hash 'type "price-list"
-           'treatment (->jsexpr/super (price-list-treatment pl))
-           'choices (->jsexpr/super (price-list-choices pl))))])
+           'name (->jsexpr/super (price-list-name pl))
+           'fixed (->jsexpr/super (price-list-fixed pl))
+           'adjustable (->jsexpr/super (price-list-adjustable pl))
+           'levels (->jsexpr/super (price-list-levels pl))
+           'answers (->jsexpr/super (price-list-answers pl))))])
 
-(define possible-treatments
-  '(((a 0) (a 1))
-    ((b 1) (b 2))))
+(define ((rw-pl pl) rw)
+  (haml
+   (:table
+    ,@(for/list ([(level i) (in-indexed (price-list-levels pl))])
+        (define field-name (format "option-~a" i))
+        (define option-by-type
+          (hash 'fixed (price-list-fixed pl)
+                'adjustable (set-level/adjustable (price-list-adjustable pl) level)))
+        (haml
+         (:tr
+          ,@(for/list ([t '(fixed adjustable)])
+              (haml
+               (:td
+                (rw field-name (lambda (name _value _errors)
+                                 (haml
+                                  (:label
+                                   (:input
+                                    ([:name name]
+                                     [:type "radio"]
+                                     [:value (~a t)]))
+                                   (describe (hash-ref option-by-type t)))))))))
+          ,@(let ([errors (rw field-name (forms:widget-errors))])
+              (if (null? errors)
+                  null
+                  `((tr (td ([colspan "2"]) ,@errors))))))))
+    (:tr
+     (:td
+      (:button
+       ([:type "submit"])
+       "Submit"))))))
 
-(define (make-price-list-options _sym n)
-  (list
-   (list (option n 0)
-         (option 0 0))
-   (list (option n 1)
-         (option 0 0))))
-
-(define (render-options optionss)  ;; ss -> list of lists of options
+(define (render-pl pl)
   (define the-form
     (forms:form
      list
-     (for/list ([(options i) (in-indexed optionss)])
+     (for/list ([(level i) (in-indexed (price-list-levels pl))])
        (define field-name
          (string->symbol (format "option-~a" i)))
-       (define option-by-index
-         (for/hash ([(o i) (in-indexed options)])
-           (values i o)))
-       (cons field-name (forms:ensure forms:binding/number
+       (define option-by-type
+         (hash 'fixed (price-list-fixed pl)
+               'adjustable (set-level/adjustable (price-list-adjustable pl) level)))
+       (cons field-name (forms:ensure forms:binding/symbol
                                       (forms:required)
                                       (lambda (v)
                                         (cond
-                                          [(hash-ref option-by-index v #f) => forms:ok]
+                                          [(hash-ref option-by-type v #f) (forms:ok (list level v))]
                                           [else (forms:err "Invalid option selected.")])))))))
 
   (haml
    (form
     the-form
     (lambda (choices)
-      (define treatments (get 'treatments))
-      (define treatment (car treatments))
-      (put 'treatments (cdr treatments))
+      ; FIXME: put relies on unique id, so name has to be unique.
+      ; Ideally this should be enforced somewhere in the design so it throws an error
+      ; before running through half the study.
+      ; TODO: Is it sensible to require it to be uniqe? If not, then it may be impossible (or hard)
+      ; to infer which specific choice this was, as was the case in study in summer 2020.
+      (put (string->symbol
+            (string-append
+             "price-list-"
+             (symbol->string (price-list-name pl))))
+            (struct-copy price-list pl (answers choices))))
+    (rw-pl pl))))
 
-      (define price-lists (get 'price-lists null))
-      (put 'price-lists (append
-                         price-lists
-                         (list (price-list treatment
-                                           (for/list ([options (in-list optionss)]
-                                                      [chosen (in-list choices)])
-                                             (choice chosen options)))))))
-    (lambda (rw)
-      (haml
-       (:table
-        ,@(for/list ([(options i) (in-indexed optionss)])
-            (define field-name (format "option-~a" i))
-            (haml
-             (:tr
-              ,@(for/list ([(o j) (in-indexed options)])
-                  (haml
-                   (:td
-                    (rw field-name (lambda (name _value _errors)
-                                     (haml
-                                      (:label
-                                       (:input
-                                        ([:name name]
-                                         [:type "radio"]
-                                         [:value (~a j)]))
-                                       (describe o))))))))
-              ,@(let ([errors (rw field-name (forms:widget-errors))])
-                  (if (null? errors)
-                      null
-                      `((tr (td ([colspan "2"]) ,@errors))))))))
-        (:tr
-         (:td
-          (:button
-           ([:type "submit"])
-           "Submit")))))))))
+;; TODO: Improve interface. In fact, option types should be definable by users, since
+;; price lists should work with all kinds of options.
+(define (make-pl #:name name
+                 #:fixed-work fixed-work
+                 #:fixed-money fixed-money
+                 #:adjustable-work adjustable-work
+                 #:levels-of-money levels-of-money)
+  (price-list name
+              (option fixed-work fixed-money)
+              (adjustable-option adjustable-work)
+              levels-of-money
+              null))
 
-(define (info-step)
-  (haml
-   (:div
-    (:h1 "You are in the Price Lists study")
-    (button
-     (lambda ()
-       (define treatments
-         (shuffle (random-ref possible-treatments)))
-
-       (put 'treatments/orig treatments)
-       (put 'treatments treatments))
-     "Continue"))))
-
-(define (price-list-step)
-  (define treatment (car (get 'treatments)))
+(define ((price-list-step pl))
   (haml
    (:div
     (:h1 "Price List")
-    (render-options
-     (apply make-price-list-options treatment)))))
+    (render-pl pl))))
 
 (define (price-list-step/bot n-fixed)
   (for ([elt (in-list (bot:find-all "tr"))]
@@ -182,16 +183,47 @@
           (element-click! (cadr radios)))))
   (element-click! (bot:find "button[type=submit]")))
 
-(define pl-study
-  (make-study
-   #:requires '()
-   #:provides '()
-   (list
-    (make-step 'info info-step)
-    (make-step 'price-list
-               price-list-step
-               #:for-bot price-list-step/bot
-               (lambda ()
-                 (if (null? (get 'treatments))
-                     next
-                     'price-list))))))
+  (define price-lists
+    (hash 'choice1 (price-list 'choice1
+                               (option 0 0)
+                               (adjustable-option 10)
+                               '(0 1 2)
+                               null)))
+
+  (define (info-step)
+    (haml
+     (:div
+      (:h1 "You are in the Price Lists study")
+      (button void "Continue"))))
+
+  (define pl-study
+    (make-study
+     #:requires '()
+     #:provides '()
+     (list
+      (make-step 'info info-step)
+      (make-step 'price-list
+                 (price-list-step (hash-ref price-lists 'choice1))
+                 #:for-bot price-list-step/bot
+                 ))))
+
+(module+ main
+
+  (define pl-bot
+    (study->bot pl-study))
+
+  ;; Default model runs default bot. Alter model to alter bot behavior.
+  (define (model id)
+    (match id
+      ['(*root* price-list)
+       0]
+
+      [_ (values)]))
+
+  (run-bot
+   #:study-url "http://127.0.0.1:5100/study/pl-test1"
+   #:username "bot@example.com"
+   #:password "password"
+   ;; #:headless? #f
+   ;; #:delay 3
+   (pl-bot model)))
