eefba7be57ed9442f19c086e7154178f057f37b8
Bogdan Popa <bogdan@defn.io>
2021-04-15 12:11:43 +0300

    bin: fixup bust-study-caches

--- bin/bust-study-caches.sh
+++ bin/bust-study-caches.sh
@@ -7,5 +7,5 @@ PKGS=$(while IFS= read -r p; do
     basename "$(dirname "$p")"
 done < <(find "$ROOT" -type f -name "info.rkt") | xargs)
 
-rm -r "$ROOT/congame/studies/compiled"
+rm -r "$ROOT/congame-web/studies/compiled"
 raco setup --pkgs $PKGS
