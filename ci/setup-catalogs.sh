1f30685e3ae0c91cde2eeb999c3de402de4ec96e
Bogdan Popa <bogdan@defn.io>
2021-05-20 11:54:52 +0300

    build: pin catalogs to tomorrow

--- ci/setup-catalogs.sh
+++ ci/setup-catalogs.sh
@@ -6,6 +6,6 @@ echo "Setting up user catalogs..."
 raco pkg config \
      --user \
      --set catalogs \
-     https://download.racket-lang.org/releases/7.9/catalog/ \
-     https://racksnaps.defn.io/built-snapshots/2020/11/12/catalog/ \
-     https://racksnaps.defn.io/snapshots/2020/11/12/catalog/
+     https://download.racket-lang.org/releases/8.1/catalog/ \
+     https://racksnaps.defn.io/built-snapshots/2021/05/21/catalog/ \
+     https://racksnaps.defn.io/snapshots/2021/05/21/catalog/
