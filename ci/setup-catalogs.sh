e0901c25debbe3517f96dca110f444059d377429
Bogdan Popa <bogdan@defn.io>
2021-08-11 11:04:04 +0300

    ci: bump package catalogs to today, drop built-snapshots

--- ci/setup-catalogs.sh
+++ ci/setup-catalogs.sh
@@ -7,5 +7,4 @@ raco pkg config \
      --user \
      --set catalogs \
      https://download.racket-lang.org/releases/8.1/catalog/ \
-     https://racksnaps.defn.io/built-snapshots/2021/08/04/catalog/ \
-     https://racksnaps.defn.io/snapshots/2021/08/04/catalog/
+     https://racksnaps.defn.io/snapshots/2021/08/11/catalog/
