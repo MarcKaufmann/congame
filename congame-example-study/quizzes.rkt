82b8b8c0ad36eb69cabb19e897e5ca16d2ae76db
Marc Kaufmann <marc.kaufmannmk@gmail.com>
2022-02-22 18:27:03 +0100

    quiz: refactor passing in questions

--- congame-example-study/quizzes.rkt
+++ congame-example-study/quizzes.rkt
@@ -3,6 +3,7 @@
 (require congame/components/formular
          congame/components/study
          congame/components/export
+         congame-web/components/identity
          koyo/haml
          racket/format
          racket/generic
@@ -14,14 +15,15 @@
  #;(struct-out quiz-question)
  )
 
-(define (start)
+(define ((start questions))
   (page
    (haml
     (.container
      (:h1 "Quiz")
      (:p "When ready, start the quiz")
-     (button void "Start Quiz")))))
-
+     (button
+      void
+      "Start Quiz")))))
 
 #;(serializable-struct quiz-question (text answer score)
                      #:transparent
@@ -49,41 +51,33 @@
 (define (make-quiz-question text answer)
   (quiz-question text answer #f))
 
-(define ((question-handler key))
-  (define q-text (question-for key))
-  (page
-   (haml
-    (.container
-     (:h3 "Question")
-     (formular
-      (haml
-       (:div
-        (#:answer (input-textarea q-text))
-        (:button.button.next-button ([:type "submit"]) "Submit")))
-      (lambda (#:answer answer)
-        (let ([q (make-quiz-question q-text answer)])
-          (put key q)
-          (put/instance
-           key
-           (hash-set
-            (get/instance key (hash))
-            (current-participant-id)
-            q)))))))))
+(define (get-answers/current-participant)
+  (hash-ref
+   (get/instance 'quiz-answers)
+   (current-participant-id)))
+
+(define (get-answer/current-participant key)
+  (hash-ref (get-answers/current-participant) key #f))
 
 (define (quiz-question-get-score key)
   (quiz-question-score
    (hash-ref
-    (get/instance key)
-    (current-participant-id))))
+    (get-answers/current-participant)
 
-(define (done)
+    'key
+    #f)))
+
+(define (final-page)
   (define scores
-    (for/list ([key (hash-keys info-econ-quiz1-questions)])
-      (quiz-question-get-score key)))
+    (for/list ([(key answer) (in-hash (hash-ref (get/instance 'quiz-answers) (current-participant-id)))])
+      (quiz-question-score answer)))
   (define total-score
     (if (andmap number? scores)
         (apply + scores)
         "Not graded"))
+  (put 'total-score total-score)
+  (put/identity 'total-score total-score)
+
   (page
    (haml
     (.container
@@ -93,68 +87,64 @@
      (:p "Your score will be updated as your questions are graded")
 
      (:h3 "Answers")
-     ,@(for/list ([key (hash-keys info-econ-quiz1-questions)])
+     ,@(for/list ([(key answer) (in-hash (hash-ref (get/instance 'quiz-answers) (current-participant-id)))])
          (define q-score
-           (quiz-question-get-score key))
+           (quiz-question-score answer))
          (haml
           (:div.quiz
-           (:h4 "Question")
-           (:p (question-for key))
-           (:h4 (format "Your Score: ~a"
-                        (if q-score q-score "Not graded")))
-           (:h4 "Answer")
+           (:ul
+            (:li (:strong "Question: ") (quiz-question-text answer))
+            (:li (:strong "Your Score: ") (if q-score (~a q-score) "Not graded)")))
+           (:h4 "Suggested Answer")
            (answer-haml-for key))))))))
 
-(define info-econ-quiz1-questions
-  (hash
-   'comparative-statics
-   (cons
-     "What does Comparative Statics mean? Explain and provide an example."
-    (haml
-     (:div
-      (:p "Comparative Statics study how some endogenous variable Y changes in equilibrium with the change in an exogenous variable X.")
-      (:p "Examples")
-      (:ul
-       (:li "How does the price of a good (endogenous) change following an increase in its production cost (assumed exogenous)?")
-       (:li "How does the wage of a worker change after a raise in the minimum wage?")))))
-   'first-wft
-   (cons
-     "What does the First Welfare Theorem say?"
-    (haml
-     (:p "There are different variations, but it says roughly that if markets are complete (a price for every good in every state), are competitive (no market power, hence price-taking), and buyers and sellers are rational and fully informed, then the market is Pareto efficient.")))))
-
 (define (question-for key)
-  (car (hash-ref info-econ-quiz1-questions key)))
+  (cdr (findf (λ (q)
+                (equal? (car q) key))
+              (get 'quiz-questions))))
+
+(define (answer-haml-for key)
+  (cadr (question-for key)))
 
 (define (question-haml-for key)
   (haml
-   (:p
-    (car (hash-ref info-econ-quiz1-questions key)))))
-
-(define (answer-haml-for key)
-  (cdr (hash-ref info-econ-quiz1-questions key)))
+   (:p (car (question-for key)))))
 
 (define (quiz-admin-handler)
 
+  (define all-answers
+    (parameterize ([current-study-stack '(*root*)])
+      (get/instance 'quiz-answers '())))
+
+  (define (all-answers-for key)
+    (for/hash ([(id answers) (in-hash all-answers)]
+               #:when (hash-has-key? answers key))
+      (values id (hash-ref answers key))))
+
   (define (display-table key)
-    (define answers
-      (parameterize ([current-study-stack '(*root*)])
-        (get/instance key (hash))))
+
     (define (update-score p new-score)
+      (define current-participant-answers
+        (hash-ref all-answers p))
+      (define new-answers
+        (hash-update current-participant-answers
+                     key
+                     (λ (q)
+                       (quiz-question
+                        (quiz-question-text q)
+                        (quiz-question-answer q)
+                        new-score))))
       (parameterize ([current-study-stack '(*root*)])
         (put/instance
-         key
-         (hash-update answers p
-                      (λ (q)
-                        (quiz-question
-                         (quiz-question-text q)
-                         (quiz-question-answer q)
-                         new-score))))))
+         'quiz-answers
+         (hash-set (get/instance 'quiz-answers (hash))
+                   p
+                   new-answers))))
+
     (define (mark-as-correct p)
       (update-score p 1))
     (define (mark-as-incorrect p)
       (update-score p 0))
-
 
     (haml
      (:div
@@ -167,7 +157,7 @@
          (:th "Answer")
          (:th "Score")
          (:th "(Re)Grade")))
-       ,@(for/list ([(p q) (in-hash answers)])
+       ,@(for/list ([(p q) (in-hash (all-answers-for key))])
            (haml
             (:tr
              (:td (~a p))
@@ -199,13 +189,51 @@
   (make-study
    "quiz-admin"
    #:provides '()
-   #:requires '()
+   #:requires '(quiz-questions)
    (list
     (make-step 'admin quiz-admin-handler))))
 
-(define info-econ-quiz1
+(define (next-question-step)
+  (define (handler)
+    (define remaining-questions (get 'quiz-questions))
+    (match-define (list next-key q-text _)
+      (car remaining-questions))
+    (page
+     (haml
+      (.container
+       (:h3 "Question")
+       (formular
+        (haml
+         (:div
+          (#:answer (input-textarea q-text))
+          (:button.button.next-button ([:type "submit"]) "Submit")))
+        (lambda (#:answer answer)
+          (let ([q (make-quiz-question q-text answer)])
+            (put 'quiz-answers
+                 (hash-set (get 'quiz-answers (hash)) next-key q))
+            (put 'quiz-questions-done
+                 (cons (car remaining-questions) (get 'quiz-questions-done '())))
+            (put 'quiz-questions (cdr remaining-questions)))))))))
+  (make-step
+   'question
+   handler
+   (λ ()
+     (if (null? (get 'quiz-questions))
+         done
+         'question))))
+
+(define quiz-study
+  ;; Should check that loq not empty, that seems a mistake always
   (make-study
-   "info-econ-quiz1"
+   "quiz-study-from-questions"
+   #:provides '(quiz-answers)
+   #:requires '(quiz-questions)
+   (list
+    (next-question-step))))
+
+(define (make-quiz-study questions)
+  (make-study
+   "quiz-study"
    #:provides '()
    #:requires '()
    (list
@@ -213,13 +241,59 @@
      'check-owner
      skip
      (λ ()
+       (put 'quiz-questions questions)
        (if (current-participant-owner?) 'quiz-admin 'start)))
-    (make-step 'start start)
-    (make-step 'question1 (question-handler 'comparative-statics))
-    (make-step 'question2 (question-handler 'first-wft))
-    (make-step 'done done)
+    (make-step 'start (start info-econ-quiz1-questions))
+    (make-step/study
+     'quiz
+     quiz-study
+     #:provide-bindings '((quiz-answers quiz-answers))
+     #:require-bindings '((quiz-questions quiz-questions))
+     (λ ()
+       (put/instance
+        'quiz-answers
+        (hash-set
+         (get/instance 'quiz-answers (hash))
+         (current-participant-id)
+         (get 'quiz-answers)))
+       'final-page))
+    (make-step 'final-page final-page)
     (make-step/study
      'quiz-admin
      quiz-admin/study
+     #:require-bindings '((quiz-questions quiz-questions))
      (λ ()
        'admin-interface)))))
+
+;;; Info Econ Quiz 1
+
+(define info-econ-quiz1-questions
+  (list
+   (list
+    'comparative-statics
+    "What does Comparative Statics mean? Explain and provide an example."
+    (haml
+     (:div
+      (:p "Comparative Statics study how some endogenous variable Y changes in equilibrium with the change in an exogenous variable X.")
+      (:p "Examples")
+      (:ul
+       (:li "How does the price of a good (endogenous) change following an increase in its production cost (assumed exogenous)?")
+       (:li "How does the wage of a worker change after a raise in the minimum wage?")))))
+   (list
+    'first-wft
+    "What does the First Welfare Theorem say?"
+    (haml
+     (:p "There are different variations, but it says roughly that if markets are complete (a price for every good in every state), are competitive (no market power, hence price-taking), and buyers and sellers are rational and fully informed, then the market is Pareto efficient.")))
+   (list
+    'different-patience
+    "Suppose that the difference in utility derives from one type being more impatient than the other. The firm is selling exactly the same product, but offers it in period 1 and period 2 for different prices. Which is the higher type: the more patient or the less patient person? Explain what defines the higher type."
+    (haml
+     (:p "The high type is the one who values " (:strong "increase") " in quality more and thus is willing to pay more for such increases. The more impatient person is the one willing to pay more to receive the good in period 1, hence they are the high type. (This does rely somewhat on the assumption that both types value no bundle or no quantity the same amount -- that they have similar outside options.)")))
+   (list
+    'information-rent
+    "Suppose that the bundles we observe are (T, q) and (T', q'), with (T, q) being for the higher type. Expressed in terms of utility of these bundles, how much information rent can the highest type make at most? What about the lowest type? Why? (Note: as in the lecture notes, we implicitly assume that each type can buy only one bundle or the other, but not both or multiples of each bundle.)"
+    (haml
+     (:p "The lowest type never gets any information rent. Letting u be the utility of the highest type, the highest type can pretend to be the lowest type to receive u(q') - T' from pretending to be the low type.")))))
+
+(define info-econ-quiz1
+  (make-quiz-study info-econ-quiz1-questions))
