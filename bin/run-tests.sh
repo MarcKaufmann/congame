3fc83f8bf9332c282548f30f6162b460dec3718c
Bogdan Popa <bogdan@defn.io>
2022-03-03 10:07:44 +0200

    test: log marionette debug output

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 Xvfb :5 -ac &
 export DISPLAY=:5
+export PLTSTDERR='error debug@marionette'
 raco test \
      /src/congame/congame-pjb-studies \
      /src/congame/congame-tests
