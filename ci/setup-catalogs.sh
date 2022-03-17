24777b6c8987d756361402a247a8a839c0a07826
Bogdan Popa <bogdan@defn.io>
2022-03-17 09:35:02 +0200

    ci: pin 2022/03/17 catalog

--- ci/setup-catalogs.sh
+++ ci/setup-catalogs.sh
@@ -7,4 +7,4 @@ raco pkg config \
      --user \
      --set catalogs \
      https://download.racket-lang.org/releases/8.4/catalog/ \
-     https://racksnaps.defn.io/snapshots/2022/03/03/catalog/
+     https://racksnaps.defn.io/snapshots/2022/03/17/catalog/
