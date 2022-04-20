e7d17b1b1bdce560a3d921b83a898707633aa691
Bogdan Popa <bogdan@defn.io>
2022-04-20 09:22:43 +0300

    identity,api: refactor put-instance-page in a more direct style

--- congame-identity/pages/api.rkt
+++ congame-identity/pages/api.rkt
@@ -4,8 +4,8 @@
          koyo/http
          koyo/json
          web-server/http
-         "../components/study-instance.rkt"
-         "../components/shadow.rkt")
+         "../components/shadow.rkt"
+         "../components/study-instance.rkt")
 
 (provide
  put-instance-page)
@@ -13,42 +13,40 @@
 ;; TODO: Look up congame-server and instance ids off of the shadow and
 ;; store them with the instance data.
 (define ((put-instance-page db) req instance-id)
-  (cond
-    [(lookup-shadow/key db (bindings-ref (request-bindings/raw req) 'key ""))
-     => (lambda (the-shadow)
-          (let/ec ret
-            (unless (equal? (shadow-instance-id the-shadow) instance-id)
-              (ret (response/json
-                    #:code 400
-                    (hasheq 'error "mismatched shadow and instance id"))))
+  (let/ec esc
+    (define the-shadow
+      (lookup-shadow/key db (bindings-ref (request-bindings/raw req) 'key "")))
+    (unless the-shadow
+      (esc (response/json
+            #:code 401
+            (hasheq 'error "invalid API key"))))
+    (unless (equal? (shadow-instance-id the-shadow) instance-id)
+      (esc (response/json
+            #:code 400
+            (hasheq 'error "mismatched shadow and instance id"))))
+    (define data
+      (string->jsexpr
+       (bytes->string/utf-8
+        (request-post-data/raw req))))
+    (define (ref id)
+      (hash-ref data id (λ () (error 'ref "field '~a' is required" id))))
 
-            (define data
-              (string->jsexpr
-               (bytes->string/utf-8
-                (request-post-data/raw req))))
-            (define (ref id)
-              (hash-ref data id (λ () (error 'ref "field '~a' is required" id))))
-
-            (with-handlers ([(lambda (e)
-                               (and (exn:fail? e)
-                                    (regexp-match? #rx"field.*required" (exn-message e))))
-                             (lambda (e)
-                               (response/json
-                                #:code 400
-                                (hasheq 'error (exn-message e))))])
-              (put-study-instance-data
-               db
-               #:user-id (shadow-user-id the-shadow)
-               #:instance-id instance-id
-               #:study-stack (for/list ([id (in-list (ref 'stack))])
-                               (string->symbol id))
-               #:key (string->symbol (ref 'key))
-               #:value (ref 'value)
-               #:congame-url (ref 'congame-url))
-              (response/json
-               #:code 201
-               (hasheq 'ok "ok")))))]
-    [else
-     (response/json
-      #:code 401
-      (hasheq 'error "invalid API key"))]))
+    (with-handlers ([(lambda (e)
+                       (and (exn:fail? e)
+                            (regexp-match? #rx"field.*required" (exn-message e))))
+                     (lambda (e)
+                       (response/json
+                        #:code 400
+                        (hasheq 'error (exn-message e))))])
+      (put-study-instance-data
+       db
+       #:user-id (shadow-user-id the-shadow)
+       #:instance-id instance-id
+       #:study-stack (for/list ([id (in-list (ref 'stack))])
+                       (string->symbol id))
+       #:key (string->symbol (ref 'key))
+       #:value (ref 'value)
+       #:congame-url (ref 'congame-url))
+      (response/json
+       #:code 201
+       (hasheq 'ok "ok")))))
