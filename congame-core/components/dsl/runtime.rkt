136a67b8916e61cf992bd2a807218f06fc9b25bf
Marc <marc@trichotomy.xyz>
2023-03-02 08:50:28 +0100

    conscript: add cons and append to ev

--- congame-core/components/dsl/runtime.rkt
+++ congame-core/components/dsl/runtime.rkt
@@ -86,7 +86,8 @@ SCRIPT
   not void
   zero? even? empty? equal?
   get put get/instance put/instance get/global put/global get/instance/global put/instance/global
-  list first second third fourth fifth sixth seventh eighth ninth tenth rest
+  list cons append
+  first second third fourth fifth sixth seventh eighth ninth tenth rest
   hash hash-ref hash-set hash-update
   add1 sub1
   ~a format number->string string->number symbol->string string->symbol
