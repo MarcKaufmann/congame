9d95c1f440014433abb7595f1ef5603697a3144b
Marc <marc@trichotomy.xyz>
2023-03-15 15:28:13 +0100

    conscript,ev: add make-list and make-vector

--- congame-core/components/dsl/runtime.rkt
+++ congame-core/components/dsl/runtime.rkt
@@ -89,9 +89,9 @@ SCRIPT
   > < >= <= zero? even? empty? equal?
   not void
   get put get/instance put/instance get/global put/global get/instance/global put/instance/global
-  list cons append map list-ref sort max min build-list range list->vector take drop
+  list cons append map list-ref sort max min build-list range list->vector take drop make-list
   first second third fourth fifth sixth seventh eighth ninth tenth rest
-  vector vector->list vector-length vector-map vector-ref vector-set! build-vector vector-copy! vector-take vector-drop vector-empty?
+  vector vector->list vector-length vector-map vector-ref vector-set! build-vector vector-copy! vector-take vector-drop vector-empty? make-vector
   hash hash-ref hash-set hash-set* hash-update
   ~a format number->string string->number symbol->string string->symbol string=?
   random shuffle
