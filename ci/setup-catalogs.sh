4f93134a821d317962096e7c33d48093b6659fb2
Marc <marc@trichotomy.xyz>
2023-04-26 08:49:24 +0200

    Revert "build: bump racket to 8.8"
    This reverts commit 6a4928e904830016f4895f21672c89c5617fa379 to deal
    with the error upon login with `hasher-make-hash`.

--- ci/setup-catalogs.sh
+++ ci/setup-catalogs.sh
@@ -6,5 +6,5 @@ echo "Setting up user catalogs..."
 raco pkg config \
      --user \
      --set catalogs \
-     https://download.racket-lang.org/releases/8.8/catalog/ \
-     https://racksnaps.defn.io/snapshots/2023/04/18/catalog/
+     https://download.racket-lang.org/releases/8.5/catalog/ \
+     https://racksnaps.defn.io/snapshots/2022/06/16/catalog/
