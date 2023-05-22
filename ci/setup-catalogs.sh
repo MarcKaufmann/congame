bb066673174525171e5dd8a725593bdc2395d3e8
Marc <marc@trichotomy.xyz>
2023-05-22 18:22:52 +0200

    ci: update catalog

--- ci/setup-catalogs.sh
+++ ci/setup-catalogs.sh
@@ -7,4 +7,4 @@ raco pkg config \
      --user \
      --set catalogs \
      https://download.racket-lang.org/releases/8.8/catalog/ \
-     https://racksnaps.defn.io/snapshots/2022/06/16/catalog/
+     https://racksnaps.defn.io/snapshots/2023/05/21/catalog/
