ab8f7b32b5722bb15fb3a3b7327cea2a6735a059
Marc <marc@trichotomy.xyz>
2023-06-17 17:17:15 +0200

    studies,edpb: flesh out stub

--- studies/excuses/edpb.rkt
+++ studies/excuses/edpb.rkt
@@ -1,29 +1,115 @@
-#lang racket/base
+#lang at-exp racket/base
 
 (require congame/components/study
          congame/components/transition-graph
-         koyo/haml)
+         koyo/haml
+         "instructions.rkt")
 
 (provide edpb-pilot)
+
+;;;;;;;;;;;;;;;; TEMPLATES ;;;;;;;;;;;;;
+;;;    Steps that are HTML only      ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (welcome)
   (page
    (haml
-    (.container
-     (:h1 "Welcome")))))
+    @.container{
+      @:h1{The Study}
 
-(define (stub)
+      @:p{Thank you for participating in this study exploring decisions over task allocations.}
+
+      @:p{The research is conducted by Marc Kaufmann and financed by the Central European University. Your participation is voluntary and you may withdraw at any time. Participation in the study is not associated with any forseeable risks or benefits beyond the monetary compensation.}
+
+      @:p{If you have concerns or questions, please contact the principal investigator, Marc Kaufmann, at @"kaufmannm@ceu.edu" or by direct message via Prolific. Report technical problems to @"admin@totalinsightmanagement.com".}
+
+      @button[void]{Continue}})))
+
+;;;;;;;;;;;;;;;; STUDIES
+
+(define ((stub title))
   (page
    (haml
     (.container
-     (:h1 "Stub")
+     (:h1 title)
      (button void "Next")))))
 
+(define (fail-ending)
+  (page
+   (haml
+    (.container
+     (:h1 "You cannot continue")
+     (:p "Unforuntately you failed the tutorial, so you cannot continue in this study. Please return the study.")))))
+
+(define (final)
+  (page
+   (haml
+    (.container
+     (:h1 "Thank you for participating")))))
+
+(define (tutorial-tasks-stub)
+  (page
+   (haml
+    (.container
+     (:h1 "Click below to pass the tutorial tasks")
+     (button (lambda ()
+               (put 'pass-tutorial? #t))
+             "Pass the tutorial")))))
+
+(define day1
+  (make-study
+   "edpb day 1"
+   #:transitions
+   (transition-graph
+    [sign-up --> work-choices
+             --> determine-choice-that-counts
+             --> work-day1
+             --> schedule-reminder-email
+             --> ,(lambda () done)])
+   (list
+    (make-step 'sign-up (stub "Signup"))
+    (make-step 'work-choices (stub "Make work choices"))
+    (make-step 'determine-choice-that-counts  (stub "Determine choice that counts"))
+    (make-step 'work-day1  (stub "Work Day 1"))
+    (make-step 'schedule-reminder-email  (stub "Schedule reminder email")))))
+
+
+(define day2
+  (make-study
+   "edpb day 2"
+   #:transitions
+   (transition-graph
+    [work-day2 --> exit-survey --> ,(lambda () done)])
+   (list
+    (make-step 'work-day2 (stub "Work day 2"))
+    (make-step 'exit-survey (stub "Exit survey")))))
+
+  
 (define edpb-pilot
   (make-study
    "edpb pilot"
    #:transitions
    (transition-graph
-    [welcome --> welcome])
+    [welcome --> instructions
+             --> hypothetical-time-choice
+             ; TODO: Do we really want tutorial tasks? The more uncertainty, the better maybe.
+             --> tutorial-tasks
+             --> consent
+             --> ,(lambda ()
+                    (cond [(get 'pass-tutorial?) (goto day1)]
+                          [else (goto fail-ending)]))]
+    [day1 --> day2 --> send-completion-email --> final]
+    [final --> final]
+    [fail-ending --> fail-ending])
+
    (list
-    (make-step 'welcome welcome))))
+    (make-step 'welcome welcome)
+    (make-step 'instructions instructions)
+    (make-step 'hypothetical-time-choice (stub "Hypothetical Time Choice"))
+    (make-step 'tutorial-tasks tutorial-tasks-stub)
+    (make-step 'consent (stub "Consent"))
+    (make-step/study 'day1 day1)
+    (make-step/study 'day2 day2)
+    (make-step 'send-completion-email (stub "Send Completion Email"))
+    (make-step 'final final)
+    (make-step 'fail-ending fail-ending))))
---
+++ studies/excuses/instructions.rkt
@@ -0,0 +1,83 @@
+#lang at-exp racket
+
+(require congame/components/study
+         koyo/haml)
+
+(provide instructions)
+
+(define config
+  (hash 'day2-date "tomorrow"
+        'session1-bonus 3
+        'session2-bonus 6
+        'estimated-performance-bonus 6
+        'estimated-time 20))
+
+(define (conf x)
+  (~a (hash-ref config x)))
+
+(define (instructions)
+  (page
+   (haml
+    @.container{
+      @:h1{Instructions and consent}
+
+      @:p{This study consists of two sessions. If you decide to take part, the first session will start immediately after you give consent to participate. You can complete the second session anytime @conf['day2-date] between 6 AM and midnight (Pacific Time). You will receive a completion bonus of @conf['session1-bonus] USD for completing the first session, plus @conf['session2-bonus] USD if you complete the second session as well. You can earn additional income that will depend on your decisions in the first session and your performance in the task that we describe below. The average additional income will be around @conf['estimated-performance-bonus] USD. The two sessions will take around @conf['estimated-time] minutes to complete each.}
+
+      @:p{In both sessions, you will have to do a series of simple tasks. The task consists of
+    [[[
+    categorizing articles based on their abstracts (a roughly 100-word summary of the article) . You will be given certain topics and you will have to choose the articles that are concerned with that particular topic. Here you can try the task:
+
+    Page with 3 abstracts and having to choose the ones that are on [ climate change ] comes here.
+
+    In both sessions you will have to categorize [ 15 ] abstracts each as a baseline. In one of the two sessions, you will also have to do some extra work, involving the categorization of around [ 30-40 ] extra abstracts.
+    ]]]}
+
+
+      @:h2{First session - TODAY}
+
+      @:p{In the first session, you will first have to make a series of decisions regarding how many extra  [ abstracts to categorize and how (i. e. which topic to select) ], and when. You can see an example choice here:}
+
+      @:ul{
+        @:li{25 [ abstracts as animal rights vs other ]  in the second session OR}
+        @:li{32 [ abstracts as information technology vs other ] in the second session}}
+
+      @:p{Sometimes both options will involve work in the second session, sometimes both in the first session, and sometimes you will have to choose between work in the first or in the second sessions.}
+
+      @:p{In some choices, you will have the opportunity to reveal a reason for or against choosing either option. In these cases, you will learn before the decision if the reasons are for or against the options. You will have to choose one option to reveal the reason for. These reasons will not provide additional information about how to do the tasks, rather they will inform you about [ other aspects of the topics you can choose from. ]}
+
+      @:p{After you have made all decisions, one of them will be randomly chosen and the option you chose will be implemented. For example, if the example choice above is the randomly chosen decision, and you chose to do 25 [ abstracts  as animal rights vs other ]  in the second session, then you will have to [ categorize ] zero extra [abstracts ] today and 25 [ abstracts categorized as animal rights vs other ] in the second session.}
+
+      @:p{After you learn the outcome of your decisions, you will have to [ categorize ] the [ 15 ] baseline [ abstracts ], and then do the amount of extra [ abstract categorization ] corresponding to the randomly chosen decision.}
+
+      @:p{When you are finished with the tasks, the first session will end and you will receive a link to the second session.}
+
+      @:h2{Second session - @conf['session2-bonus]}
+
+      @:p{In the second session, first you will have to [ categorize ] the [ 15 ] baseline [ abstracts ], and then you will have to do the amount of extra [ abstract categorization ] corresponding to the randomly chosen decision. After you are finished with the tasks, we will ask you to answer a couple of questions about how you made your decisions, and about how well a couple of sentences describe your behavior or attitude in some situations.}
+
+
+      @:h2{Payment}
+
+      @:p{The payment for the study will consist of three parts:}
+
+      @:ul{
+        @:li{First session: a completion bonus of [ 3 ] USD for completing the first session}
+        @:li{Second session: a completion bonus of [ 6 ] USD for completing the second session}
+        @:li{Additional income: [ 0.2 ] USD for each [ correctly categorized abstract ] - this includes the baseline [ 15 abstracts ]  each session and the extra [ abstracts ] that correspond to the randomly chosen decision.}}
+
+      @:p{You will receive all payments (completion bonuses plus additional income) via the Prolific payment system, within three days after completing the second session. If you only complete the first session, you will only receive the completion bonus for the first session, within three days after the corresponding second session.}
+
+
+      @:h2{Further information}
+
+      @:p{The purpose of this research is to examine how people make decisions for work over time.}
+
+      @:p{This study is conducted by Flora Drucker and Marc Kaufmann and financed by Central European University. If you accept to participate in the study, you may still change your mind and quit at any time. However, please note that you will only receive the completion bonus for any session if you complete it, and you will only receive the additional income if you complete both sessions.}
+
+      @:p{Participation in this study is not associated with any foreseeable risk or benefit. Your answers will be collected confidentially and anonymously (the researchers will not be able to link decisions and participants' identity beyond the Prolific ID provided). At the data analysis stage your Prolific ID will be changed to a random identifying number, and the Prolific IDs will be deleted. In case the results of the study are published, there can be no references to your identity. Data anonymity is guaranteed.}
+
+      @:p{This study received a research ethics approval from the Ethical Research Committee of Central European University.}
+
+      @:p{If you have any questions or concerns regarding this study, please contact us at @"admin@totalinsightmanagement.com" or @"lucafloradrucker.research@gmail.com".}
+
+      @button[void]{Continue}})))
