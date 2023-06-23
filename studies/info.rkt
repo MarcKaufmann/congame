ef2f2b78bf8a1301017d7bb59c936fcebc20535c
Marc <marc@trichotomy.xyz>
2023-06-23 10:49:47 +0200

    studies: define package deps at top level
    This should fix the build problem that `csv-reading` - required by
    `studies/excuses` - was not found, by moving the dependency to the top
    level. The ci pipeline only install the `studies` collection, and
    apparently that does not mean that it automatically installs the
    dependencies of every package in the collection.

---
+++ studies/info.rkt
@@ -0,0 +1,13 @@
+#lang info
+
+(define collection "studies")
+(define deps '("base"
+               "component-lib"
+               "congame-core"
+               "congame-web"
+               "csv-reading"
+               "gregor-lib"
+               "koyo-lib"
+               "sentry-lib"
+               "web-server-lib"))
+(define build-deps '())
