9d224a90e3ae0adfb84b1c71b78aee80c4c4176b
Marc <marc@trichotomy.xyz>
2023-07-07 18:18:06 +0200

    wip: split study into intro and main
    Intro exists to elicit the patience level and tell them how to signup.
    The patience level is elicited here to see if we lose impatient people
    due to requiring signup. The signup is needed so that people can resume
    the study on a later day, since the study requires multiple days of
    participation.

--- studies/excuses/edpb.rkt
+++ studies/excuses/edpb.rkt
@@ -10,42 +10,20 @@
          "templates.rkt"
          "abstract-categorization.rkt")
 
-(provide edpb-pilot)
+(provide
+ edpb-intro
+ edpb-main)
+
+;; TODO
+;; - Add intro-completion-code to the instance level of edpb-intro and edpb-main (where people receive it if they continue with the study)
+;; - Add information to the study and to the consent form that they need to signup with their prolific email to do the main study.
+;; - Change the study so that people have to sign up right away, after providing just their prolific ID and their answer to the patience question (to see selection). That way, we don't need to merge any of the data across studies and I do not have to wait for Bogdan to implement anything. We should still implement talking between instances.
+;; - create a prolific only signup page, i.e. people have to type in a prolific email or ID. This avoids them signing up with personal emails.
 
 ;;;;;;;;;;;;;;;; TEMPLATES ;;;;;;;;;;;;;
 ;;;    Steps that are HTML only      ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define (welcome)
-  (page
-   (haml
-    @.container{
-      @:h1{The Study}
-
-      @:p{Thank you for participating in this study examining how people make decisions for work over time.}
-
-      @:p{This study is conducted by Flora Drucker and Marc Kaufmann and financed by Central European University. Your participation is voluntary and if you accept to participate, you may withdraw at any time. However, please note that you will receive some bonuses if you complete specific parts of the study. This is described in more detail on the Instructions page.}
-
-      @:p{Participation in this study is not associated with any foreseeable risk or benefit. Your answers will be collected confidentially and anonymously (the researchers will not be able to link decisions and participants' identity beyond the Prolific ID provided). At the data analysis stage your Prolific ID will be changed to a random identifying number, and the Prolific IDs will be deleted. In case the results of the study are published, there can be no references to your identity. Data anonymity is guaranteed.}
-
-      @:p{This study received a research ethics approval from the Ethical Research Committee of Central European University.}
-
-      @:p{If you have any questions or concerns regarding this study, please contact us at @"admin@totalinsightmanagement.com" or @"lucafloradrucker.research@gmail.com".}
-
-      @button[void]{Continue}})))
-
-(define (hypothetical-time-choice)
-  (page
-   (haml
-    (.container
-     (formular
-      (haml
-       (:div
-        (#:patience
-         (input-number
-          "How willing are you to give up something that is beneficial for you today in order to benefit more from that in the future? (0 means not willing at all, 10 means very willing)"
-          #:min 0 #:max 10))
-        submit-button)))))))
 
 (define ((stub title))
   (page
@@ -148,7 +126,86 @@
 
      @:p{Unfortunately, you failed the comprehension test too many times. Please return the study.}))))
 
-;;;;;;;;;;;;;;;; STUDIES
+
+
+(define (intro-completion-code)
+  (page
+   (haml
+    (.container
+     (:h1 "Your Completion Code")
+
+     (:p (format "To complete the introduction, go now to prolific and enter the following completion code: ~a" (get/instance 'completion-code)))
+
+     (button void "Continue")))))
+
+(define (thank-you)
+  (page
+   (haml
+    (.container
+     (:h1 "Thank you!")
+
+     (:p "Thank you for having participated in the study.")))))
+
+;;;;;;;;;;;;;;;; INTRO STUDY
+
+(define (landing-page)
+  (page
+   (haml
+    @.container{
+      @:h1{The Study}
+
+      @:p{Thank you for participating in this study examining how people make decisions for work over time.}
+
+      @:p{This study is conducted by Flora Drucker and Marc Kaufmann and financed by Central European University. Your participation is voluntary and if you accept to participate, you may withdraw at any time. However, please note that you will receive some bonuses if you complete specific parts of the study. This is described in more detail on the Instructions page.}
+
+      @:p{Participation in this study is not associated with any foreseeable risk or benefit. Your answers will be collected confidentially and anonymously (the researchers will not be able to link decisions and participants' identity beyond the Prolific ID provided). At the data analysis stage your Prolific ID will be changed to a random identifying number, and the Prolific IDs will be deleted. In case the results of the study are published, there can be no references to your identity. Data anonymity is guaranteed.}
+
+      @:p{This study received a research ethics approval from the Ethical Research Committee of Central European University.}
+
+      @:p{If you have any questions or concerns regarding this study, please contact us at @"admin@totalinsightmanagement.com" or @"lucafloradrucker.research@gmail.com".}
+
+      (formular
+       (haml
+        (:div
+         (#:prolific-ID (input-text "What is your Prolific ID?"))
+         (#:patience
+          (input-number
+           "How willing are you to give up something that is beneficial for you today in order to benefit more from that in the future? (0 means not willing at all, 10 means very willing)"
+           #:min 0 #:max 10))
+
+         submit-button)))})))
+
+(define (signup)
+  (page
+   (haml
+    (.container
+     (:h1 "Sign up for Login")
+
+     (:p "Since part of the study is today and some in the future, you will need to sign up with your Prolific email, so that you can resume the study at any time without losing your progress. To do so, please follow the following steps:")
+
+     (:ul
+      (:li "Go to the signup page")
+      (:li "Type in your prolific email (it is your Prolific ID followed by ...)")
+      (:li "Type in a password")
+      (:li "You will receive a validation email on Prolific. Click on or copy-paste the validation link therein to validate your account")
+      (:li "Log in to your account and start the study <study-name>"))
+
+     ; FIXME: Add an instructions video on how to do it.
+
+     ))))
+
+(define edpb-intro
+  (make-study
+   "edpb pilot"
+   #:transitions
+   (transition-graph
+    [landing-page --> signup --> signup])
+
+   (list
+    (make-step 'landing-page landing-page)
+    (make-step 'signup signup))))
+
+;;; MAIN STUDY
 
 (define (comprehension-test-success?)
   (> (get 'comprehension-test-score) 0))
@@ -165,9 +222,7 @@
    "edpb-tutorial"
    #:transitions
    (transition-graph
-    [initialize --> welcome
-                --> hypothetical-time-choice
-                --> instructions
+    [initialize --> instructions
                 --> task-description
                 ; TODO: Do we really want tutorial tasks? The more uncertainty, the better maybe.
                 --> tutorial-tasks
@@ -189,9 +244,7 @@
 
    (list
     (make-step 'initialize initialize)
-    (make-step 'welcome welcome)
     (make-step 'instructions instructions)
-    (make-step 'hypothetical-time-choice hypothetical-time-choice)
     ; TODO: Is this type of thunk a good way to hook up functional pages with data? If so, provide syntactic sugar for it.
     (make-step
      'task-description
@@ -221,7 +274,7 @@
              --> schedule-reminder-email
              --> ,(lambda () done)])
    (list
-    (make-step 'sign-up (stub "Signup"))
+
     (make-step 'work-choices (stub "Make work choices"))
     (make-step 'determine-choice-that-counts  (stub "Determine choice that counts"))
     (make-step 'work-day1  (stub "Work Day 1"))
@@ -280,9 +333,10 @@
          (error 'switch-phase-to "failed because the current phase is ~a, but needs to be ~a to switch phase" old-phase cp)]))
 
 
-(define edpb-pilot
+
+(define edpb-main
   (make-study
-   "edpb pilot"
+   "edpb main study"
    #:transitions
    (transition-graph
     [assigning-roles --> ,(lambda ()
@@ -298,9 +352,10 @@
     [waiting-page --> tutorial
                   --> consent
                   --> ,(lambda ()
-                         (cond [(get 'consent-given?) (goto day1)]
-                               [else (goto no-consent-ending)]))]
-
+                         (cond [(get 'consent-given?) (goto signup)]
+                               [else (goto intro-completion-code)]))]
+    [intro-completion-code --> thank-you
+                           --> thank-you]
     [day1 --> day2 --> send-completion-email --> final]
     [final --> final]
     [no-consent-ending --> no-consent-ending])
@@ -313,8 +368,10 @@
     (make-step 'waiting-page waiting-page)
     (make-step 'error-page (stub "Error page"))
     (make-step 'consent consent)
+    (make-step 'intro-completion-code intro-completion-code)
+    (make-step 'admin admin)
+    (make-step/study 'abstracts-admin abstracts-admin)
     (make-step/study 'day1 day1)
     (make-step/study 'day2 day2)
     (make-step 'send-completion-email (stub "Send Completion Email"))
-    (make-step 'final final)
-    (make-step 'no-consent-ending no-consent-ending))))
+    (make-step 'final final))))
