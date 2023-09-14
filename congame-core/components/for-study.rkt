d1bf60bfc5dd35abea49e0ff5d8465bcead952e0
Marc <marc@trichotomy.xyz>
2023-09-14 16:16:29 +0200

    for-study: add #:provides to return bindings
    Note however that `for/study` seems to be broken, and doesn't store
    values at the right stack level, instead saving on the parent level.

--- congame-core/components/for-study.rkt
+++ congame-core/components/for-study.rkt
@@ -10,11 +10,14 @@
 
 (define-syntax (for/study stx)
   (syntax-parse stx
-    [(_ {~optional {~and #:substudies use-substudies?}} clauses body-e ... tail-e)
+    [(_ {~optional {~and #:substudies use-substudies?}}
+        {~optional {~seq #:provides provides:expr}}
+        clauses body-e ... tail-e)
      #:with ((pre-body-e ...)
              (post-body-e ...))
      (split-for-body stx #'(body-e ... tail-e))
      #`(do-for-study
+        {~? {~@ #:provides provides}}
         #,(if (attribute use-substudies?)
               #'make-step/study
               #'make-step)
@@ -24,8 +27,9 @@
           pre-body-e ...
           (cons (λ () post-body-e ...) procs)))]))
 
-(define (do-for-study make-step-proc procs)
+(define (do-for-study make-step-proc procs #:provides [provides '()])
   (make-study
    "for/study"
+   #:provides provides
    (for/list ([(proc i) (in-indexed (in-list procs))])
      (make-step-proc (string->symbol (format "iter-~a" i)) proc))))
