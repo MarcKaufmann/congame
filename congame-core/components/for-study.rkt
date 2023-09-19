fdfe90289096d20e71196d9fe5a40fdc00e88271
Bogdan Popa <bogdan@defn.io>
2023-09-19 10:07:12 +0300

    core,for-study: add #:requires

--- congame-core/components/for-study.rkt
+++ congame-core/components/for-study.rkt
@@ -11,12 +11,14 @@
 (define-syntax (for/study stx)
   (syntax-parse stx
     [(_ {~optional {~and #:substudies use-substudies?}}
+        {~optional {~seq #:requires requires:expr}}
         {~optional {~seq #:provides provides:expr}}
         clauses body-e ... tail-e)
      #:with ((pre-body-e ...)
              (post-body-e ...))
      (split-for-body stx #'(body-e ... tail-e))
      #`(do-for-study
+        {~? {~@ #:requires requires}}
         {~? {~@ #:provides provides}}
         #,(if (attribute use-substudies?)
               #'make-step/study
@@ -27,9 +29,12 @@
           pre-body-e ...
           (cons (λ () post-body-e ...) procs)))]))
 
-(define (do-for-study make-step-proc procs #:provides [provides '()])
+(define (do-for-study make-step-proc procs
+                      #:requires [requires null]
+                      #:provides [provides null])
   (make-study
    "for/study"
+   #:requires requires
    #:provides provides
    (for/list ([(proc i) (in-indexed (in-list procs))])
      (make-step-proc (string->symbol (format "iter-~a" i)) proc))))
