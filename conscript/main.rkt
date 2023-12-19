b9cffc95fd309dbeea8a459fe377f35ea4ca91e8
Bogdan Popa <bogdan@defn.io>
2023-12-19 10:01:59 +0200

    conscript: add note about why we can't sandbox #lang conscript effectively

--- conscript/main.rkt
+++ conscript/main.rkt
@@ -1,5 +1,16 @@
 #lang racket/base
 
+;; SECURITY: This is the entrypoint for the restricted conscript
+;; language. This language is not allowed to import arbitrary racket
+;; modules and care must be taken to avoid giving it too much power (eg.
+;; the ability to interact with the congame database at a level beyond
+;; basic gets and puts -- we don't want #lang conscript study writers
+;; to be able to drop the database). We can't sandbox #lang conscript
+;; studies because our architecture is such that certain modules (db,
+;; congame/core/study, and others) would need to be shared with the
+;; sandbox, thereby giving #lang conscript study writers a way to get to
+;; the database and do dangerous things.
+
 (module reader syntax/module-reader
   conscript/base
   #:read (lambda (in) (do-read-syntax #f in))
