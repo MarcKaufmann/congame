4f67b85bbdf0d09c34873dee316ea7701ae81a13
Bogdan Popa <bogdan@defn.io>
2024-01-15 11:16:11 +0200

    conscript: add warning to whitelist

--- conscript/base.rkt
+++ conscript/base.rkt
@@ -55,6 +55,9 @@
  defvar)
 
 (begin-for-syntax
+  ;; Be careful what you provide here. Avoid providing access to
+  ;; things that might allow the user to "escape" #lang conscript, eg.
+  ;; racket/system, ffi/unsafe or any system-level functionality.
   (define whitelist
     '(racket/format
       racket/list
--- conscript/module-whitelist.rktd
+++
@@ -1,3 +0,0 @@
-;; Be careful what you provide here. Avoid providing access to
-;; things that might allow the user to "escape" #lang conscript, eg.
-;; racket/system, ffi/unsafe or any system-level functionality.
