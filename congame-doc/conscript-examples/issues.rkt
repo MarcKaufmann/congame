a7cea22d99b1fefb928f52ea93de8ec93d7a5534
Marc <marc@trichotomy.xyz>
2024-02-21 14:32:10 +0100

    conscript example: studies dealing with issues students are having

---
+++ congame-doc/conscript-examples/issues.rkt
@@ -0,0 +1,66 @@
+#lang conscript
+
+;; How to build pages that are similar.
+
+;; Approach 1: define reusable html snippets.
+(define common-instructions
+  @md*{
+    # Instructions
+
+    In this study you will do several things:
+
+    1. This
+    2. and that
+    3. and even this})
+
+(defstep (instructions1)
+  @md{
+    @common-instructions
+
+    @button{Continue}})
+
+(defstep (instructions2)
+  @md{
+    @common-instructions
+
+    You also should be doing this:
+
+    4. More,
+    5. and more,
+    6. and even more!!
+
+    @button{Continue}})
+
+(define (end)
+  @md{
+    # Game Over})
+
+(defstudy reuse-instructions
+  [instructions1 --> instructions2
+                 --> ,(lambda () done)])
+
+;; Approach 2: define a function that returns a step with a variable part, and then give different names in the transition graph of the study
+
+(defstep ((hello-message m))
+  @md{
+      # Hello!
+
+      We are so @m to have you here!
+
+      @button{Continue}})
+
+(defstudy reuse-welcome
+  [[happy-hello (hello-message "happy")] --> [thrilled-hello (hello-message "thrilled")]
+                                         --> [meh-hello (hello-message "meh")]
+                                         --> ,(lambda () done)])
+
+;; Both approach 1 and 2 assume that you know in advance what the variable part is. So this does not work if, say, the variable part is something that the participant submits themselves, such as their own name.
+;; For that, you have to ask the participant, store it in the DB, and then display it as shown in the tutorial.
+
+(provide
+ reuse-all)
+
+(defstudy reuse-all
+  [reuse-instructions --> reuse-welcome
+                      --> end]
+  [end --> end])
