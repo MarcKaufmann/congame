5cd866338c63b01da4b3e7a3ffba1460d7b45766
Marc Kaufmann <marc.kaufmannmk@gmail.com>
2024-02-27 22:25:45 +0100

    conscript: utility to create list of sliders
    Why does it not work to replace `name` by `kwd` and transform names
    already to keywords via `(map string->keyword (build-list ...))` and
    plug `kwd` into the template? It errors with list not accepting a
    keyword argument, which makes sense given that we create `(#:slider-0
    ...)`, but why does it not fail in this version? Some quotes/unquotes?

--- conscript/utils.rkt
+++ conscript/utils.rkt
@@ -1,7 +1,6 @@
 #lang racket/base
 
 (require (for-syntax racket/base
-                     racket/syntax
                      syntax/parse/pre)
          congame/components/formular
 
@@ -9,10 +8,18 @@
 
 (define-syntax (make-sliders stx)
   (syntax-parse stx
-    [(_ (list var ...))
-     #'(list
-        (haml
-         (:div
-          (list (string->keyword (symbol->string var ...)) (input-number "Number") ))))]))
+    [(_ n:nat)
+     (with-syntax ([(name ...)
+                    (build-list
+                     (syntax-e #'n)
+                     (lambda (i) (format "slider-~a" i)))])
+     #'(haml
+        (:div
+         (:div ([:class "slider"])
+          (list
+           (string->keyword name)
+           (input-range "")
+           (haml
+            (:span "Value: " (:output ""))))) ... )))]))
 
-(make-sliders '(a b c))
+(make-sliders 2)
