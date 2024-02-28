9def4aba2174173eaa2d824d5816188ca6f4e705
Marc <marc@trichotomy.xyz>
2024-02-28 08:35:35 +0100

    conscript: two slider makers
    However they return different values, are both fine?
    
    '(div
      ()
      (div ((class "slider")) (#:slider-0 #<procedure:...onents/formular.rkt:426:2> (span () "Value: " (output () ""))))
      (div ((class "slider")) (#:slider-1 #<procedure:...onents/formular.rkt:426:2> (span () "Value: " (output () "")))))
    '(div
      ()
      (haml (:div ((:class "slider")) (list #:slider-0 (input-range "") (haml (:span "Value: " (:output ""))))))
      (haml (:div ((:class "slider")) (list #:slider-1 (input-range "") (haml (:span "Value: " (:output "")))))))

--- conscript/utils.rkt
+++ conscript/utils.rkt
@@ -9,17 +9,81 @@
 (define-syntax (make-sliders stx)
   (syntax-parse stx
     [(_ n:nat)
-     (with-syntax ([(name ...)
-                    (build-list
-                     (syntax-e #'n)
-                     (lambda (i) (format "slider-~a" i)))])
-     #'(haml
-        (:div
-         (:div ([:class "slider"])
-          (list
-           (string->keyword name)
-           (input-range "")
-           (haml
-            (:span "Value: " (:output ""))))) ... )))]))
+     (with-syntax ([(kwd ...)
+                    (map
+                     string->keyword
+                     (build-list
+                      (syntax-e #'n)
+                      (lambda (i) (format "slider-~a" i))))])
+       #'(haml
+          (:div
+           (:div ([:class "slider"])
+                 (list
+                  'kwd
+                  (input-range "")
+                  (haml
+                   (:span "Value: " (:output ""))))) ... )))]))
 
 (make-sliders 2)
+
+;(make-sliders 2 slider-builder)
+
+#;(define-syntax (make-sliders-2 stx)
+  (syntax-parse stx
+    [(_ n:nat tmp)
+     (with-syntax ([(kwd ...)
+                    (map
+                     string->keyword
+                     (build-list
+                      (syntax-e #'n)
+                      (lambda (i) (format "slider-~a" i))))])
+       #'(haml
+          (:div
+           tmp ... )))]))
+
+#;(make-sliders-2
+ 2
+ (:div ([:class "slider"])
+       (list
+        kwd
+        (input-range "")
+        (haml
+         (:span "Value: " (:output ""))))))
+
+
+(define-for-syntax (make-slider kwd)
+  `(haml
+    (:div ([:class "slider"])
+          (list
+           ,kwd
+           (input-range "")
+           (haml
+            (:span "Value: " (:output "")))))))
+
+(define-syntax (make-sliders-3 stx)
+  (syntax-parse stx
+    [(_ n:nat)
+     (with-syntax ([(kwd ...)
+                    (map
+                     string->keyword
+                     (build-list
+                      (syntax-e #'n)
+                      (lambda (i) (format "slider-~a" i))))])
+       #`(haml
+          (:div
+           (#,make-slider 'kwd) ... )))]))
+
+(make-sliders-3 2)
+
+(define-syntax (make-sliders-4 stx)
+  (syntax-parse stx
+    [(_ n:nat)
+     (with-syntax ([(kwd ...)
+                    (map
+                     string->keyword
+                     (build-list
+                      (syntax-e #'n)
+                      (lambda (i) (format "slider-~a" i))))])
+       #`(haml
+          (:div
+           (#,make-slider 'kwd) ... )))]))
