63c6726f727f5f9dfa2b54c48f4e342da7c27f85
Marc <marc@trichotomy.xyz>
2024-03-15 13:35:33 +0100

    cs-survey-tools: add mathjax directly
    To avoid dependency on congame-web/ where mathjax-scripts is defined.

--- conscript/survey-tools.rkt
+++ conscript/survey-tools.rkt
@@ -7,7 +7,6 @@
          (prefix-in congame: congame/components/study)
          (submod congame/components/study accessors)
          congame/components/resource
-         congame-web/components/template
          koyo/haml
          racket/list
          racket/runtime-path)
@@ -129,3 +128,13 @@
 
 (provide
  mathjax-scripts)
+
+(define (mathjax-scripts)
+  (haml
+   (:div
+    (:script
+     ([:src "https://polyfill.io/v3/polyfill.min.js?features=es6"]))
+    (:script
+     ([:id "MathJax-script"]
+      [:async ""]
+      [:src "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"])))))
