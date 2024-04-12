5fbc6cc764d9df324400f41ea42784a86d26924c
Bogdan Popa <bogdan@defn.io>
2024-04-12 10:10:23 +0300

    ci: bump package catalogs for gregor fix

--- ci/setup-catalogs.sh
+++ ci/setup-catalogs.sh
@@ -7,4 +7,4 @@ raco pkg config \
      --user \
      --set catalogs \
      https://download.racket-lang.org/releases/8.11/catalog/ \
-     https://racksnaps.defn.io/snapshots/2024/03/02/catalog/
+     https://racksnaps.defn.io/snapshots/2024/04/12/catalog/
