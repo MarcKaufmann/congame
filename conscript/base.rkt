e62881831f52941b61e51d127b6065ba88499f29
Marc <marc@trichotomy.xyz>
2024-10-02 10:27:33 +0200

    conscript: provide map-{result*,validator} and some list functions

--- conscript/base.rkt
+++ conscript/base.rkt
@@ -55,7 +55,7 @@
  lambda λ
  void sleep
  for for/fold for/list in-range in-inclusive-range in-list
- list list? list* null null? cons pair? car cdr map member for-each shuffle length
+ list list? list* null null? cons pair? car cdr map member for-each shuffle length findf filter
  display displayln print println printf eprintf write writeln
 
  in-hash
