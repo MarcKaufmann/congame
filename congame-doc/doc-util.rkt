22ac3432b69cc47a4bd60ff464b2255adb1667a4
Joel Dueck <joel@jdueck.net>
2024-10-12 10:33:58 -0500

    Add mark and mock textboxes

--- congame-doc/doc-util.rkt
+++ congame-doc/doc-util.rkt
@@ -13,6 +13,13 @@
 (define-runtime-path congame-css "aux.css")
 (define-runtime-path congame-tex "aux.tex")
 
+;; Mark text as worthy of review for possible Congame improvements
+(define (mark . elems)
+  (element (style "review" (list (css-style-addition congame-css)
+                                 (alt-tag "mark")))
+           elems))
+
+;; Style for sample terminal output 
 (define (terminal . args)
   (compound-paragraph (style "terminal" (list (color-property (list #x66 #x33 #x99))
                                               (css-style-addition congame-css)
@@ -29,19 +36,27 @@
 (define (rem . args)
   (apply racketcommentfont (cons "# " args)))
 
+;; Style text as a keyboard key or a button
 (define (kbd . elems)
   (element (style "kbd" (list (css-style-addition congame-css)
                               (alt-tag "kbd")))
            elems))
 
+;; Style for output in the DrRacket interactions window
 (define (dr-message . elems)
   (element (style "dr-message" (list (css-style-addition congame-css)
                                      (alt-tag "span")))
            elems))
 
+;; Simulate a browser window
 (define (browser . elems)
   (compound-paragraph
    (style "browser" (list (css-style-addition congame-css)
                           (alt-tag "div")
                           (tex-addition congame-tex)))
    (decode-flow elems)))
+
+(define (mock-textbox)
+  (element (style "mock-textbox" (list (css-style-addition congame-css)
+                                       (alt-tag "span")))
+           " "))
