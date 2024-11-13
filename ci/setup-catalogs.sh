2418038aec9abb32551d0ea46abd62db01ca8492
Bogdan Popa <bogdan@defn.io>
2024-11-13 08:19:00 +0200

    ci: bump catalog version

--- ci/setup-catalogs.sh
+++ ci/setup-catalogs.sh
@@ -7,4 +7,4 @@ raco pkg config \
      --user \
      --set catalogs \
      https://download.racket-lang.org/releases/8.14/catalog/ \
-     https://racksnaps.defn.io/snapshots/2024/09/30/catalog/
+     https://racksnaps.defn.io/snapshots/2024/11/13/catalog/
