02b3ea26bd8cdafe56f7facf63d0be1f4466cd45
Bogdan Popa <bogdan@defn.io>
2024-11-20 09:14:09 +0200

    conscript: drop circular dep on congame-web

--- conscript/info.rkt
+++ conscript/info.rkt
@@ -2,16 +2,13 @@
 
 (define collection "conscript")
 (define deps
-  '("congame-web"
-    "base"
+  '("base"
     "at-exp-lib"
     "buid-lib"
     "commonmark-lib"
     "congame-core"
     "forms-lib"
     "koyo-lib"
-    "mime-type-lib"
-    "net-lib"
     "threading-lib"
     "web-server-lib"))
 (define build-deps
