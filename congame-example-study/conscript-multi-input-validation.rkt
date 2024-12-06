1eede2019139daec2c8e0cc914e907a693b00bf2
Marc <marc@trichotomy.xyz>
2024-12-06 16:20:10 +0100

    examples: how to validate multi-input lists (WIP)
    This is currently broken, either because:
    
    - `map-validator` and `input-list` are not supposed to use like this; or
    - `map-validator` and `input-list` are not playing well together
    
    The validation works the first time, but if there is an error, then the
    newly rendered screen only shows the first box, presumably because the
    three answers were combined into a single list. Displaying again would
    require deconstructing the list.

---
+++ congame-example-study/conscript-multi-input-validation.rkt
@@ -0,0 +1,45 @@
+#lang conscript
+
+(provide
+ sum-percentages)
+
+(defvar beliefs)
+
+(define (input-percent label)
+  (input-number label #:min 0 #:max 100))
+
+(defstep (distribute-100)
+  @md{# Distribute 100
+
+      One of A, B, or C must happen, so the probabilities must sum to 100.
+
+      @form{
+
+            @div{
+                 @set![beliefs
+                       (map-validator
+                        (input-list
+                         (list
+                          (input-percent "How likely is A?")
+                          (input-percent "How likely is B?")
+                          (input-percent "How likely is C?")))
+                        (lambda (loa)
+                          (define total (apply + loa))
+                          (if (= total 100)
+                              `(ok loa)
+                              `(err "Your answers must sum to 100."))))]}
+            @submit-button}})
+
+
+(defstep (display-answers)
+  (define belief-a (first beliefs))
+  (define belief-b (second beliefs))
+  (define belief-c (third beliefs))
+  (define belief-total (+ belief-a belief-b belief-c))
+
+  @md{# Answers
+
+      In total we get @(~a belief-a) + @(~a belief-b) + @(~a belief-c) = @(~a belief-total).})
+
+(defstudy sum-percentages
+  [distribute-100 --> display-answers --> display-answers])
