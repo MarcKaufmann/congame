685cea16393780e26d774a4ba1672d45ce895f3f
Joel Dueck <joel@jdueck.net>
2025-02-27 09:56:40 -0600

    Doc check tool tweaks

--- congame-doc/doc-check/main.rkt
+++ congame-doc/doc-check/main.rkt
@@ -20,6 +20,7 @@
    conscript/markdown
    conscript/matchmaking
    conscript/resource
+   conscript/survey-tools
    conscript/tasks
    conscript/tool))
 
@@ -31,8 +32,7 @@
 ;; Each element in the list is either: a module path (= ignore all bindings in that module)
 ;;                                     list of module path and ids (= ignore just those ids)
 (ignored-bindings
- '(conscript/html-element
-   (congame/components/transition-graph --> goto)))
+ '((congame/components/transition-graph --> goto)))
 
 ;; Any bindings coming from racket/*, as well as those in the “ignore” list above,
 ;; are filtered out.
@@ -87,9 +87,9 @@
   (for ([section (in-list test-results)])
     (match-define (test mod total missing) section)
     (unless (null? missing)
-      (term-bytes color:red color:bold)
+      ;(term-bytes color:red color:bold)
       (write-string (scribble-module mod))
-      (term-bytes color:reset color:yellow)
+      ;(term-bytes color:reset color:yellow)
       (for ([b (in-list missing)])
         (write-string (stub-scribble-def b)))))
   (term-bytes color:reset))
