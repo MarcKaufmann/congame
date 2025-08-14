0744b0b55bda5dc57da81e1666ddae90b47e5acd
Bogdan Popa <bogdan@defn.io>
2025-08-14 11:33:43 +0300

    web+identity: require koyo-lib 0.40+ for deploy command

--- ci/setup-catalogs.sh
+++ ci/setup-catalogs.sh
@@ -7,4 +7,4 @@ raco pkg config \
      --user \
      --set catalogs \
      https://download.racket-lang.org/releases/8.17/catalog/ \
-     https://racksnaps.defn.io/snapshots/2025/06/26/catalog/
+     https://racksnaps.defn.io/snapshots/2025/08/14/catalog/
