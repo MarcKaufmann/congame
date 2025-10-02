3ffef14b97f670a8fc3a32dd66a8983385f475f1
Bogdan Popa <bogdan@defn.io>
2025-10-02 11:15:14 +0300

    web: bump connection pool size to 100

--- congame-web/dynamic.rkt
+++ congame-web/dynamic.rkt
@@ -59,6 +59,7 @@
             (make-broker db))]
   [broker-admin (broker) (make-broker-admin-factory "/admin/jobs")]
   [db (make-database-factory
+       #:max-connections 100
        (lambda ()
          (trace-connection
           (postgresql-connect
