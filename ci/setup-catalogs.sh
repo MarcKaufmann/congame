9418c75a1601ceb98116746a5e4cd0805afd0a7e
Bogdan Popa <bogdan@defn.io>
2025-10-02 12:10:45 +0300

    build: bump racket to 8.18

--- ci/setup-catalogs.sh
+++ ci/setup-catalogs.sh
@@ -6,5 +6,5 @@ echo "Setting up user catalogs..."
 raco pkg config \
      --user \
      --set catalogs \
-     https://download.racket-lang.org/releases/8.17/catalog/ \
+     https://download.racket-lang.org/releases/8.18/catalog/ \
      https://racksnaps.defn.io/snapshots/2025/09/08/catalog/
